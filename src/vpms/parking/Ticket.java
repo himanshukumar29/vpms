6951a895b8eff0285620c6606e84a6f5dde5503d
Himanshu Kumar <102627080+himanshukumar29@users.noreply.github.com>
2025-06-30 12:04:16 +0530

    Add files via upload

--- src/vpms/parking/Ticket.java
+++ src/vpms/parking/Ticket.java
@@ -5,12 +5,15 @@ import vpms.model.Vehicle;
 public class Ticket {
     private Vehicle vehicle;
     private long entryTime;
+    private ParkingSpot spot;
 
-    public Ticket(Vehicle vehicle) {
+    public Ticket(Vehicle vehicle, ParkingSpot spot) {
         this.vehicle = vehicle;
+        this.spot = spot;
         this.entryTime = System.currentTimeMillis();
     }
 
-    public long getEntryTime() { return entryTime; }
     public Vehicle getVehicle() { return vehicle; }
+    public long getEntryTime() { return entryTime; }
+    public ParkingSpot getSpot() { return spot; }
 }
